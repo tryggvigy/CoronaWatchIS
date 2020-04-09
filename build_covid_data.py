04aa1a045d624f973da4130379a86745f575aa51
Tryggvi Gylfason <tryggvi@spotify.com>
2020-04-09 21:24:01 +0000

    Add SYNI and ALL sheets

--- build_covid_data.py
+++ build_covid_data.py
@@ -5,12 +5,16 @@ BASE_URL = "https://docs.google.com/spreadsheets/d/1xgDhtejTtcyy6EN5dbDp5W3TeJhK
 SMIT_SHEET = f"{BASE_URL}&gid=0"
 SPITALI_SHEET = f"{BASE_URL}&gid=1167203444"
 ALDUR_SHEET = f"{BASE_URL}&gid=2147090221"
+SYNI_SHEET = f"{BASE_URL}&gid=1883412372"
+ALLT_SHEET = f"{BASE_URL}&gid=1788393542"
 
 def build_data():
     print("Downloading")
     cases_resp=requests.get(url=SMIT_SHEET)
     hospital_resp=requests.get(url=SPITALI_SHEET)
     age_response=requests.get(url=ALDUR_SHEET)
+    testing_response=requests.get(url=SYNI_SHEET)
+    all_response=requests.get(url=ALLT_SHEET)
 
     # create data folder
     Path('data').mkdir(parents=True, exist_ok=True)
@@ -20,6 +24,8 @@ def build_data():
     open(str(Path('data', 'covid_in_is.cvs')), "wb").write(cases_resp.content)
     open(str(Path('data', 'covid_in_is_hosp.cvs')), "wb").write(hospital_resp.content)
     open(str(Path('data', 'covid_in_is_age.cvs')), "wb").write(age_response.content)
+    open(str(Path('data', 'covid_in_is_testing.cvs')), "wb").write(testing_response.content)
+    open(str(Path('data', 'covid_in_is_all.cvs')), "wb").write(all_response.content)
 
     print("Success")
 
